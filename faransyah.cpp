6a5c664858facb4572bf9af88cae26a873597c44
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-03-13 16:11:02 +0700

    example 1 dan 2

--- faransyah.cpp
+++ faransyah.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    cout << "Hello World!" << endl;
-    return 0;
-}
+int main()
+{ // begin
+  // numeric
+  int bil;
+  // character status
+  string status;
+  // display "masukkan bilangan = "
+  cout << "masukkan bilangan = ";
+  // accept bil
+  cin >> bil; 
+  // if(bil == 0) 
+  if (bil == 0) 
+  // status = "nol"
+  status = "nol"; 
+  // else if (bil %2 == 0)
+  else if (bil % 2 == 0)
+  // status = "genap"
+  status = "genap";
+  // else
+  else
+  //status = "ganjil"
+  status = "ganjil";
+  // display "bilangannya"
+  cout << "bilangannya =" << status;
+} //end
